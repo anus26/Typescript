260b77521c54131956ccace00cdf817f5d2953e6
anas-raza~ <anusraza268@gmail.com>
2024-06-28 12:01:01 -0700

    index file created

---
+++ app.ts
@@ -0,0 +1,17 @@
+
+// TS Compiler P
+// Type Annotations
+// Type Inference
+// Type Aliases
+// Union Type
+
+
+// type annotations
+// let username:string= 'abdullah';
+// username= 'abdullah';
+// console.log(username);
+
+
+// let age:number = 30
+// age = 4
+// console.log(age);
