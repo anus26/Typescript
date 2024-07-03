5d51ad6bd15afeae4627bbf82ea9cf103be9a219
anas-raza~ <anusraza268@gmail.com>
2024-07-02 22:56:53 -0700

    modified

--- app.ts
+++ app.ts
@@ -15,3 +15,59 @@
 // let age:number = 30
 // age = 4
 // console.log(age);
+
+// let isLoggedIn:boolean=true
+// console.log(isLoggedIn);
+
+// let user:string[]=['Ahmed raza']
+// let any
+// let username:any='raza'
+// username=20
+
+
+
+
+// function greetuser(username:string,age:number) {
+//     console.log('hello ${username} with age ${age}');
+    
+// }
+//  greetuser('anas',30)
+
+//  typeinterface
+// let username='abdulqadir'
+//  username=18      
+
+// let num =20
+//  num='age'
+
+
+//  let fruits=['mango',25]
+
+
+
+// type alicases
+
+//  type Abd=string
+//  let user:Abd='nagori'
+
+// type fruits=(string | number)[]
+// let fruits:fruits=['khan',29,]
+
+
+// type User={
+//     username:string;
+//     email:string;
+//     passward:number;
+//     isLoggedIn:true;
+//     lastname?:string
+// }
+
+// const user : User={
+//     username:'tabriz',
+//     email:'tabriznagori234.com',
+//     passward:786,
+//     isLoggedIn:true
+
+// }
+// user.lastname='khan';
+// console.log(user);
